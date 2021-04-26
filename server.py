35cae010feb400c1d52b1fa61dd7bfd38d58af2b
Степан <gretoton@mail.ru>
2021-04-26 20:47:27 +0300

    ex 3

--- server.py
+++ server.py
@@ -13,5 +13,10 @@ def profession(profs):
     return render_template('index.html', prof=profs)
 
 
+@app.route('/list_prof/<list>')
+def list_of_professions(list):
+    return render_template('index.html', list=list)
+
+
 if __name__ == '__main__':
     app.run(port=5000, host='127.0.0.1')
